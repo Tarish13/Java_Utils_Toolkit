a0d40b1b29a321029f4127c08c3f7d696b524583
Tarish13 <cucharin13@gmail.com>
2025-11-27 20:05:57 +0100

    Practica inicial. Refrescar conceptos basicos de Java como librerias, JavaDoc, y operaciones matematicas sencillas.

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Conversiones c = new Conversiones();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        System.out.println( c.celsiusToFahrenheit(32));
+        System.out.println( c.fahrenheitToCelsius(89.6));
+        System.out.println( c.minutesToSeconds(10));
+        System.out.println( c.secondsToHours(3600));
+        System.out.println( c.secondsToHours(5896));
+        System.out.println( c.secondsToHours(145896));
+
+        System.out.println( c.percent(480, 1200));
+        System.out.println( c.addPercentage(100,50));
+        System.out.println( c.addPercentage(78,20));
+        System.out.println( c.addPercentage(432,200));
     }
 }
+
